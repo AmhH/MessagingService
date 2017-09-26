a903db07f1aa51c1f5e03821045af0acf2c18b38
Amh <abaams78@gmail.com>
2017-09-26 10:12:28 -0500

    mongoose module

--- Project/server/routes/model.js
+++ Project/server/routes/model.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/MessaginService');
+mongoose.connect('mongodb://localhost:27017/MessaginService',{useMongoClient: true});
 //mongoose.connect('mongodb://username:password@host:port/?database?option...');
 var Schema = mongoose.Schema;
 
-var addressSchema = new Schema({ 
+var Address = new Schema({ 
                             street: String,
                             city: String,
                             state: String,
@@ -11,7 +11,7 @@ var addressSchema = new Schema({
                             country: String
                          });
 
-var messageSchema = new Schema({ 
+var Message = new Schema({ 
                             mailer: {type: String, index: true},
                             receiver: {type: String, index: true},
                             subject: String,
@@ -22,13 +22,13 @@ var messageSchema = new Schema({
                             label: String
                          });
 
-var contactSchema = new Schema({ 
-                            fullname: {type: String, index: true, unique: true},
-                            email: {type: String, index: true},
+var Contact = new Schema({ 
+                            fullname: {type: String, index: true, unique: true, sparse: true},
+                            email: {type: String, index: true, sparse: true},
                             phone: String
                          });
 
-var credentialSchema = new Schema({ 
+var UserCredential = new Schema({ 
                             userName: {type: String, index: true, unique: true},
                             password: String
                          });
@@ -38,7 +38,7 @@ var userSchema = new Schema({
                             lName: {type: String, index: true},
                             dob: Date,
                             phone: String,
-                            credential: Credential,
+                            userCredential: UserCredential,
                             address: Address,
                             messages: [Message],
                             contacts: [Contact]                            
@@ -46,16 +46,157 @@ var userSchema = new Schema({
 
 //userSchema.index({fName:1, credential:userName})
 
-var Address = mongoose.model('User', addressSchema);
-var Credential = mongoose.model('Credential', credentialSchema);
+/*var Address = mongoose.model('User', addressSchema);
+//var UserCredential = mongoose.model('UserCredential', credentialSchema);
 var Message = mongoose.model('Message', messageSchema);
-var Contact = mongoose.model('Contact', contactSchema);                       
+var Contact = mongoose.model('Contact', contactSchema);  */                   
 var User = mongoose.model('User', userSchema);
 
+var aman = new User({
+    fName: 'aman',
+    lName: 'aman',
+    dob: new Date('09/09/2000'),
+    phone: '123456789',
+    /*userCredential: credential1,
+    address: add1,
+    messages: [message1B,message2S,message2T]*/
+});
+
+var ameha = new User({
+    fName: 'ameha',
+    lName: 'ameha',
+    dob: new Date('09/09/2000'),
+    phone: '123456789',
+    /*userCredential: credential2,
+    address: add2,
+    messages: [message1S,message2B,message1T]*/
+});
+
+var mera = new User({
+    fName: 'mera',
+    lName: 'mera',
+    dob: new Date('09/09/2000'),
+    phone: '123456789',
+    /*userCredential: credential3,
+    address: add2*/
+});
+
+aman.address = {
+    street: '1000 4th st',
+    city: 'Fairfiled',
+    state: 'IA',
+    zipCode: 52556,
+    country: 'USA'
+};
+
+ameha.address = {
+    street: '465 Main st',
+    city: 'Fairfiled',
+    state: 'IA',
+    zipCode: 52558,
+    country: 'USA'
+};
+
+mera.address = {
+    street: '465 Main st',
+    city: 'Fairfiled',
+    state: 'IA',
+    zipCode: 52558,
+    country: 'USA'
+};
+
+aman.userCredential = {
+    userName: 'aman',
+    password: 'aman'
+};
+
+ameha.userCredential = {
+    userName: 'ameha',
+    password: 'ameha'
+};
+
+mera.userCredential = {
+    userName: 'mera',
+    password: 'mera'
+};
+
+aman.messages.push({
+    mailer: 'ameha',
+    receiver: 'aman',
+    subject: 'Hello',
+    content: 'Welcome To our mailing service',
+    date: new Date(),
+    isread: false,
+    type: 'primary',
+    label: 'inbox'
+});
+
+ameha.messages.push({
+    mailer: 'ameha',
+    receiver: 'aman',
+    subject: 'Hello',
+    content: 'Welcome To our mailing service',
+    date: new Date(),
+    isread: false,
+    type: 'primary',
+    label: 'sent'
+});
+
+ameha.messages.push({
+    mailer: 'aman',
+    receiver: 'ameha',
+    subject: 'Thank you',
+    content: 'I Love your application and it is a funtastic idea ',
+    date: new Date(),
+    isread: true,
+    type: 'primary',
+    label: 'inbox'
+});
+
+aman.messages.push({
+    mailer: 'aman',
+    receiver: 'ameha',
+    subject: 'Thank you',
+    content: 'I Love your application and it is a funtastic idea ',
+    date: new Date(),
+    isread: true,
+    type: 'primary',
+    label: 'sent'
+});
+
+ameha.messages.push({
+    mailer: 'aman',
+    receiver: 'ameha',
+    subject: 'Notication',
+    content: 'how to use our app ',
+    date: new Date(),
+    isread: true,
+    type: 'primary',
+    label: 'trash'
+});
+
+aman.messages.push({
+    mailer: 'ameha',
+    receiver: 'aman',
+    subject: 'Notication',
+    content: 'how to use our app ',
+    date: new Date(),
+    isread: true,
+    type: 'primary',
+    label: 'trash'
+});
+
 module.exports = {
+                    User,
+                    aman,
+                    ameha,
+                    mera
+                };
+
+/*module.exports = {
                     User,
                     Address,
                     Message,
                     Contact,
                     Credential
-                };
+                };*/
